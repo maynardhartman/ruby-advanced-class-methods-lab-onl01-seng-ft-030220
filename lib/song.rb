bf1f8518c0a9ac371bb28a9a375b91505329dbe8
Maynard Hartman <mjhartman@brighthouse.com>
2020-04-07 12:39:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,12 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize(name, artist_name)
+    @name = name
+    @artist_name = artist_name
+    @@all << self
+  end
+  
   def self.all
     @@all
   end
